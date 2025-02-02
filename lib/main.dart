81fb34bbd89f28260d3a74e90a52d5e3c30834cc
Shakirullah <omotososakiru25@gmail.com>
2025-02-02 10:01:53 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
 // Root of the widget
 import 'package:flutter/material.dart';
+import 'package:fylo_data_storage_component/colors.dart';
 import 'package:fylo_data_storage_component/responsive_layout.dart';
 
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
+    Future<void> _loadResources() async {
+    // Simulating a delay for loading resources
+    await Future.delayed(const Duration(seconds: 2));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,7 +22,22 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
         primaryColor: Colors.black,
       ),
-      home:  ResponsiveLayout(),
+      home:  FutureBuilder(
+        future: _loadResources(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return const ResponsiveLayout();
+          } else {
+            return const Scaffold(
+              body: Center(
+                child: CircularProgressIndicator(
+                  valueColor: AlwaysStoppedAnimation<Color>(AppColors.darkBlue),
+                ),
+              ),
+            );
+          }
+        },
+      ),
     );
   }
 }
